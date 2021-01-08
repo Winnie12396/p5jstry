9f98aaad9928d04bd13d502c867ac757aa9af6d2
Winnie12396 <71814998+Winnie12396@users.noreply.github.com>
2021-01-08 13:12:51 +0800

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <Canvas />
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Canvas from '@/components/Canvas' 
+// Note: @ refers to the /src/ folder
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    Canvas,
+  }
 }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="canvas"></div>
+</template>
+
+<script>
+import P5 from 'p5' // Package from npm
+
+export default {
+  name: 'App',
+  mounted() {
+    const script = p5 => {
+      let x = 760;
+      let y = 350;
+      let easing = 0.1;
+      // These are your typical setup() and draw() methods
+      p5.setup = () => {
+        p5.createCanvas(1520, 700);
+        p5.noStroke();
+      };
+      p5.draw = () => {
+        //p5.clear();
+        //p5.ellipse(p5.mouseX, p5.mouseY, 80, 80);
+        p5.background('powderblue');
+
+        let targetX = p5.mouseX;
+        let dx = targetX - x;
+        x += dx * easing;
+
+        let targetY = p5.mouseY;
+        let dy = targetY - y;
+        y += dy * easing;
+
+        
+        p5.ellipse(x, y, 100, 100);
+        
+      }; 
+    }
+    // Attach the canvas to the div
+    const p5canvas = new P5(script, 'canvas');
+  },
+  
+}
+</script>
+
+<style></style>
